9926de640040091c66eaba0eecdd4bf85fa5b88f
samssi <samir.hamadi@gmail.com>
2024-01-03 10:51:33 +0200

    Add routes-package

--- gogo-rust-rest-app/src/main.rs
+++ gogo-rust-rest-app/src/main.rs
@@ -1,47 +1,6 @@
-use axum::http::StatusCode;
-use axum::response::{IntoResponse, Response};
-use axum::{Json, Router};
-use axum::routing::get;
-use serde::Serialize;
-use crate::ApiResponse::JsonData;
+use crate::routes::routes::router;
 
-#[derive(Serialize)]
-struct Message {
-    message: String
-}
-
-enum ApiResponse {
-    Ok,
-    JsonData(Message)
-}
-
-impl IntoResponse for ApiResponse {
-    fn into_response(self) -> Response {
-        match self {
-            ApiResponse::Ok => (StatusCode::OK).into_response(),
-            JsonData(message) => (StatusCode::OK, Json(message)).into_response()
-        }
-    }
-}
-
-async fn read_message() -> ApiResponse {
-    let message = Message {
-        message: String::from("Hi there!")
-    };
-
-    JsonData(message)
-}
-
-async fn health_check() -> ApiResponse {
-    ApiResponse::Ok
-}
-
-fn router() -> Router {
-    Router::new()
-        .route("/health", get(health_check))
-        .route("/v1/messages", get(read_message))
-        // .route("/v1/messages", post(post_message))
-}
+mod routes;
 
 #[tokio::main]
 async fn main() {
