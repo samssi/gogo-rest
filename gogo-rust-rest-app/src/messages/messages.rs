64c081f59211d48209376240b36acab51ac555d9
samssi <samir.hamadi@gmail.com>
2024-01-04 19:47:00 +0200

    Add app state to contain db connection pool

--- gogo-rust-rest-app/src/messages/messages.rs
+++ gogo-rust-rest-app/src/messages/messages.rs
@@ -1,13 +1,23 @@
+use axum::extract::State;
 use axum::Json;
 use serde::{Deserialize, Serialize};
-use crate::routes::response::ApiResponse;
+use crate::routes::response::{ApiResponse, AppState};
 
 #[derive(Serialize,Deserialize,Debug)]
 pub struct Message {
     pub message: String
 }
 
-pub async fn read_message() -> ApiResponse {
+pub async fn read_message(State(app_state): State<AppState>) -> ApiResponse {
+    let connection = app_state.db_pool.get().await.unwrap();
+    for i in 1..10 {
+        let stmt = connection.prepare_cached("SELECT 1 + $1").await.unwrap();
+        let rows = connection.query(&stmt, &[&i]).await.unwrap();
+        let value: i32 = rows[0].get(0);
+        assert_eq!(value, i + 1);
+        println!("{:?}", value);
+    }
+
     let message = Message {
         message: String::from("Hi there!")
     };
