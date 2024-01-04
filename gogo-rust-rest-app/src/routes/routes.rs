b5a6e40948f515fbe514c7e4e8be573d1f811257
samssi <samir.hamadi@gmail.com>
2024-01-04 18:47:18 +0200

    Add initial Postgres pool implementation

--- gogo-rust-rest-app/src/routes/routes.rs
+++ gogo-rust-rest-app/src/routes/routes.rs
@@ -2,7 +2,6 @@ use axum::{Router};
 use axum::routing::get;
 use crate::health::health::health_check;
 use crate::messages::messages::{read_message, create_message};
-use crate::routes::response::ApiResponse;
 pub fn router() -> Router {
     Router::new()
         .route("/health/", get(health_check))
