2fd5c4502f6785eb3a538a43195fd60c5e804b62
samssi <samir.hamadi@gmail.com>
2025-07-01 16:32:16 +0300

    Get rid of the ApiResponse wrapper. It is a bad idea

--- gogo-rust-rest-app/src/health/router.rs
+++ gogo-rust-rest-app/src/health/router.rs
@@ -1,12 +1,16 @@
-use crate::core::axum::{ApiResponse, AppState};
+use crate::core::axum::AppState;
+use axum::http::StatusCode;
+use axum::response::IntoResponse;
 use axum::routing::get;
-use axum::Router;
+use axum::{Json, Router};
+use serde_json::json;
 use std::sync::Arc;
 
 pub fn create_health_router() -> Router<Arc<AppState>> {
     Router::new().route("/health", get(health_check))
 }
 
-pub async fn health_check() -> ApiResponse {
-    ApiResponse::Ok
+pub async fn health_check() -> impl IntoResponse {
+    let payload = json!({ "status": "Ok" });
+    (StatusCode::OK, Json(payload))
 }
