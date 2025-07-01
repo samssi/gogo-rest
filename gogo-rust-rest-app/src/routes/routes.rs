6f3cdb032dbe5075fec617fe7bc693254d372ceb
samssi <samir.hamadi@gmail.com>
2025-07-01 12:13:40 +0300

    Use mod-file structure

--- gogo-rust-rest-app/src/routes/routes.rs
+++ gogo-rust-rest-app/src/routes/routes.rs
@@ -1,17 +1,17 @@
-use axum::{Router};
-use axum::routing::get;
 use crate::db::db::init_deadpool;
 use crate::health::health::health_check;
-use crate::messages::messages::{read_message, create_message};
+use crate::messages::messages::{create_message, read_message};
 use crate::routes::response::AppState;
+use axum::routing::get;
+use axum::Router;
 
 pub async fn router() -> Router {
     let state = AppState {
-        db_pool: init_deadpool()
+        db_pool: init_deadpool(),
     };
 
     Router::new()
-        .route("/health/", get(health_check))
-        .route("/v1/messages/", get(read_message).post(create_message))
+        .route("/health", get(health_check))
+        .route("/v1/messages", get(read_message).post(create_message))
         .with_state(state)
 }
