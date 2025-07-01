310ee55214e4de36b05466e77d2d43eb257dcef4
samssi <samir.hamadi@gmail.com>
2025-07-01 16:09:54 +0300

    Remove unwraps, move common froms to core

--- gogo-rust-rest-app/src/core/errors.rs
+++ gogo-rust-rest-app/src/core/errors.rs
@@ -1,5 +1,6 @@
 use axum::http::StatusCode;
 use axum::response::{IntoResponse, Response};
+use deadpool_postgres::PoolError;
 use std::fmt;
 
 #[derive(Debug)]
@@ -9,6 +10,19 @@ pub enum ApplicationError {
 
 pub enum DatabaseError {
     Postgres(tokio_postgres::Error),
+    PostgresPoolError(PoolError),
+}
+
+impl From<tokio_postgres::Error> for DatabaseError {
+    fn from(error: tokio_postgres::Error) -> DatabaseError {
+        DatabaseError::Postgres(error)
+    }
+}
+
+impl From<PoolError> for DatabaseError {
+    fn from(value: PoolError) -> Self {
+        DatabaseError::PostgresPoolError(value)
+    }
 }
 
 pub enum AxumApplicationError {
@@ -20,7 +34,8 @@ pub enum AxumApplicationError {
 impl fmt::Display for DatabaseError {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
         match self {
-            DatabaseError::Postgres(err) => write!(f, "Postgres error: {}", err),
+            DatabaseError::Postgres(error) => write!(f, "Postgres error: {}", error),
+            DatabaseError::PostgresPoolError(error) => write!(f, "Postgres pool error: {}", error),
         }
     }
 }
