9ed82002238668304a275a93cc73146f5e6c90e7
samssi <samir.hamadi@gmail.com>
2025-07-02 13:32:16 +0300

    Add dummy tonic runner and task

--- gogo-rust-rest-app/src/main.rs
+++ gogo-rust-rest-app/src/main.rs
@@ -4,6 +4,7 @@ use crate::core::errors::ApplicationError;
 use crate::health::router::create_health_router;
 use crate::messages::router::create_messages_router;
 use std::sync::Arc;
+use std::time::Duration;
 
 mod core;
 mod health;
@@ -28,11 +29,20 @@ async fn run_axum(listener_address: &str, state: Arc<AppState>) -> Result<(), Ap
     Ok(())
 }
 
+async fn run_tonic(listener_address: &str, state: Arc<AppState>) -> Result<(), ApplicationError> {
+    loop {
+        tokio::time::sleep(Duration::from_secs(5)).await;
+        println!("dummy tonic is running...");
+    }
+}
+
 #[tokio::main]
 async fn main() -> Result<(), ApplicationError> {
     let state = create_state()?;
     let axum_state = state.clone();
     let axum_task = tokio::spawn(async move { run_axum("0.0.0.0:3000", axum_state).await });
+
+    let tonic_task = tokio::spawn(async move { run_tonic("0.0.0.0:3001", state).await });
 
     axum_task
         .await
