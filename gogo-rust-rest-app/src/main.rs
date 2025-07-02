d7192838e6c608586c7f5387a8d87bbf402377ee
samssi <samir.hamadi@gmail.com>
2025-07-02 14:09:33 +0300

    Add cancellation tokens

--- gogo-rust-rest-app/src/main.rs
+++ gogo-rust-rest-app/src/main.rs
@@ -5,6 +5,7 @@ use crate::health::router::create_health_router;
 use crate::messages::router::create_messages_router;
 use std::sync::Arc;
 use std::time::Duration;
+use tokio_util::sync::CancellationToken;
 
 mod core;
 mod health;
@@ -17,7 +18,11 @@ fn create_state() -> Result<Arc<AppState>, ApplicationError> {
     Ok(state)
 }
 
-async fn run_axum(listener_address: &str, state: Arc<AppState>) -> Result<(), ApplicationError> {
+async fn run_axum(
+    listener_address: &str,
+    state: Arc<AppState>,
+    cancellation_token: CancellationToken,
+) -> Result<(), ApplicationError> {
     let listener = tokio::net::TcpListener::bind(listener_address).await?;
 
     let routes = create_health_router()
@@ -25,14 +30,36 @@ async fn run_axum(listener_address: &str, state: Arc<AppState>) -> Result<(), Ap
         .with_state(state);
 
     println!("Axum application listening on {}", listener_address);
-    axum::serve(listener, routes).await?;
-    Ok(())
+    tokio::select! {
+        result = axum::serve(listener, routes) => {
+            result.map_err(|e| ApplicationError::StartupError(e.to_string()))
+        }
+        _ = cancellation_token.cancelled() => {
+            println!("Axum server received cancellation signal");
+            Ok(())
+        }
+    }
 }
 
-async fn run_tonic(listener_address: &str, state: Arc<AppState>) -> Result<(), ApplicationError> {
-    loop {
-        tokio::time::sleep(Duration::from_secs(5)).await;
-        println!("dummy tonic is running...");
+async fn run_tonic(
+    listener_address: &str,
+    state: Arc<AppState>,
+    cancellation_token: CancellationToken,
+) -> Result<(), ApplicationError> {
+    println!("Tonic server will listen on {}", listener_address);
+
+    tokio::select! {
+        _ = async {
+            loop {
+                tokio::time::sleep(Duration::from_secs(5)).await;
+                println!("dummy tonic is running...");
+            }
+        } => Ok(()), // This branch never ends unless cancelled
+
+        _ = cancellation_token.cancelled() => {
+            println!("Tonic server received cancellation signal");
+            Ok(())
+        }
     }
 }
 
@@ -40,9 +67,20 @@ async fn run_tonic(listener_address: &str, state: Arc<AppState>) -> Result<(), A
 async fn main() -> Result<(), ApplicationError> {
     let state = create_state()?;
     let axum_state = state.clone();
-    let axum_task = tokio::spawn(async move { run_axum("0.0.0.0:3000", axum_state).await });
+    let cancellation_token = CancellationToken::new();
 
-    let tonic_task = tokio::spawn(async move { run_tonic("0.0.0.0:3001", state).await });
+    let axum_cancellation_token = cancellation_token.clone();
+    let tonic_cancellation_token = cancellation_token.clone();
+
+    let axum_task =
+        tokio::spawn(
+            async move { run_axum("0.0.0.0:3000", axum_state, axum_cancellation_token).await },
+        );
+
+    let tonic_task =
+        tokio::spawn(
+            async move { run_tonic("0.0.0.0:3001", state, tonic_cancellation_token).await },
+        );
 
     axum_task
         .await
