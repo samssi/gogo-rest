e2ab26013575f46b552283cb90e23d22b1796334
samssi <samir.hamadi@gmail.com>
2025-07-10 16:05:40 +0300

    Add cargo installation for protoc-gen-prost-crate

--- tools/src/proto_gen/buf.rs
+++ tools/src/proto_gen/buf.rs
@@ -6,43 +6,20 @@ use tokio::fs as async_fs;
 use tokio::io::AsyncWriteExt;
 
 pub struct Buf {
+    pub monorepo_root_dir: PathBuf,
     pub tools_bin_dir: PathBuf,
     pub buf_executable_path: PathBuf,
 }
 
 impl Buf {
-    async fn download_prost_crate(tools_bin_dir: &PathBuf) {
-        // TODO: should be installed with cargo
-        // let prost_crate_file = tools_bin_dir.join("prost-crate");
-        // let prost_executable_path = tools_bin_dir.join("protoc-gen-prost-crate");
-        //
-        // #[cfg(target_os = "macos")]
-        // println!("macos prost");
-        // let prost_url = "https://github.com/dflemstr/prost-crate/releases/latest/download/prost-crate-Darwin-aarch64";
-        //
-        // #[cfg(target_os = "linux")]
-        // let prost_url = "https://github.com/dflemstr/prost-crate/releases/latest/download/prost-crate-Linux-x86_64";
-        //
-        // #[cfg(target_os = "windows")]
-        // let prost_url = "https://github.com/dflemstr/prost-crate/releases/latest/download/prost-crate-Windows-x86_64.exe";
-        //
-        // println!("Downloading prost-crate from: {prost_url}");
-        //
-        // let bytes = reqwest::get(prost_url)
-        //     .await
-        //     .expect("Failed to download prost-crate")
-        //     .bytes()
-        //     .await
-        //     .expect("Failed to read prost-crate binary");
-        //
-        // async_fs::File::create(&prost_executable_path)
-        //     .await
-        //     .expect("Failed to create prost-crate binary")
-        //     .write_all(&bytes)
-        //     .await
-        //     .expect("Failed to write prost-crate binary");
+    pub fn download_protoc_gen_prost_crate(monorepo_root_dir: &PathBuf) {
+        let status = Command::new("cargo")
+            .args(["install", "protoc-gen-prost-crate", "--root", ".tools"])
+            .current_dir(monorepo_root_dir)
+            .status()
+            .expect("Failed to run cargo install for protoc-gen-prost-crate");
 
-        // set_permissions(&prost_executable_path, 0o755)
+        assert!(status.success(), "cargo install failed");
     }
 
     async fn download_buf() -> Buf {
@@ -50,7 +27,6 @@ impl Buf {
             find_root(Path::new(".monoreporoot")).expect("Could not find .monoreporoot");
         let tools_bin_dir = monorepo_root.join(".tools/bin");
         let buf_executable_path = tools_bin_dir.join("buf");
-        // let buf_executable_path = monorepo_root.join("buf");
 
         if !buf_executable_path.exists() {
             println!(
@@ -97,23 +73,20 @@ impl Buf {
         }
 
         Self {
+            monorepo_root_dir: monorepo_root,
             buf_executable_path,
             tools_bin_dir,
         }
     }
 
-    async fn download_extensions(tools_bin_dir: &PathBuf) {
-        // Self::download_prost_crate(tools_bin_dir).await;
+    fn download_extensions(monorepo_root_dir: &PathBuf) {
+        Self::download_protoc_gen_prost_crate(monorepo_root_dir);
     }
 
     pub async fn exec_buf(&self) -> ExitStatus {
-        // let buf_gen_root =
-        //     find_root(Path::new("buf.gen.yaml")).expect("Could not find buf.gen.yaml");
         let monorepo_root =
             find_root(Path::new(".monoreporoot")).expect("Could not find .monoreporoot");
-
         let buf_gen_root = monorepo_root.join("gogo-rust-rest-app");
-
         let buf_yaml = buf_gen_root.join("buf.gen.yaml");
         let protos_dir = monorepo_root.join("protos");
 
@@ -123,16 +96,10 @@ impl Buf {
         println!("Protos dir:   {}", protos_dir.display());
         println!("Tools dir:   {}", self.tools_bin_dir.display());
 
-        let args = [
-            "generate",
-            "--template",
-            buf_yaml.to_str().unwrap(),
-            // "--path",
-            // protos_dir.to_str().unwrap(),
-        ];
+        let args = ["generate", "--template", buf_yaml.to_str().unwrap()];
 
         println!(
-            "Command: {} {}",
+            "Running command: {} {}",
             self.buf_executable_path.display(),
             args.join(" ")
         );
@@ -158,8 +125,7 @@ impl Buf {
     pub async fn new() -> Self {
         println!("Starting buf code generation process...");
         let buf = Self::download_buf().await;
-        Self::download_extensions(&buf.tools_bin_dir).await;
-
+        Self::download_extensions(&buf.monorepo_root_dir);
         buf
     }
 }
